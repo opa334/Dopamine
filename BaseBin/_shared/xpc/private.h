7266eb921cab990097549b9f78da95a2545e3020
opa334 <opa334@users.noreply.github.com>
2023-04-06 12:56:55 +0200

    Fix XPC memory leaks

--- BaseBin/_shared/xpc/private.h
+++ BaseBin/_shared/xpc/private.h
@@ -1,10 +1,31 @@
-int xpc_pipe_receive(mach_port_t port, xpc_object_t* message);
+extern XPC_RETURNS_RETAINED xpc_object_t xpc_pipe_create_from_port(mach_port_t port, uint32_t flags);
+extern XPC_RETURNS_RETAINED xpc_object_t xpc_array_create_empty(void);
+extern XPC_RETURNS_RETAINED xpc_object_t xpc_dictionary_create_empty(void);
+extern int xpc_pipe_simpleroutine(xpc_object_t pipe, xpc_object_t message);
+extern int xpc_pipe_routine_reply(xpc_object_t reply);
 void xpc_dictionary_get_audit_token(xpc_object_t xdict, audit_token_t *token);
-int xpc_pipe_routine_reply(xpc_object_t reply);
-xpc_object_t xpc_dictionary_create_empty(void);
-xpc_object_t xpc_array_create_empty(void);
-xpc_object_t xpc_pipe_create_from_port(mach_port_t port, uint32_t flags);
-int xpc_pipe_routine(xpc_object_t pipe, xpc_object_t request, xpc_object_t* reply);
-int xpc_pipe_routine_with_flags(xpc_object_t xpc_pipe, xpc_object_t inDict, xpc_object_t *reply, uint32_t flags);
-kern_return_t xpc_pipe_routine_async(xpc_object_t pipe, xpc_object_t request, mach_port_t reply_port);
 char *xpc_strerror (int);
+
+#if __has_feature(objc_arc)
+extern int xpc_pipe_routine(xpc_object_t pipe, xpc_object_t message, XPC_GIVES_REFERENCE xpc_object_t *reply);
+#else
+extern int xpc_pipe_routine(xpc_object_t pipe, xpc_object_t message, xpc_object_t *reply);
+#endif
+
+#if __has_feature(objc_arc)
+extern int xpc_pipe_routine(xpc_object_t pipe, xpc_object_t message, XPC_GIVES_REFERENCE xpc_object_t *reply);
+#else
+extern int xpc_pipe_routine(xpc_object_t pipe, xpc_object_t message, xpc_object_t *reply);
+#endif
+
+#if __has_feature(objc_arc)
+extern int xpc_pipe_routine_with_flags(xpc_object_t xpc_pipe, xpc_object_t inDict, XPC_GIVES_REFERENCE xpc_object_t *reply, uint32_t flags);
+#else
+extern int xpc_pipe_routine_with_flags(xpc_object_t xpc_pipe, xpc_object_t inDict, xpc_object_t *reply, uint32_t flags);
+#endif
+
+#if __has_feature(objc_arc)
+extern int xpc_pipe_receive(mach_port_t port, XPC_GIVES_REFERENCE xpc_object_t *message);
+#else
+extern int xpc_pipe_receive(mach_port_t port, xpc_object_t *message);
+#endif
