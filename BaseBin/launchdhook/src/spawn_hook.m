984bdb7e9f254a3a747114895300b1ac9ad6379a
opa334 <opa334@users.noreply.github.com>
2023-04-18 15:12:34 +0200

    add some debug code for tracing process launches

--- BaseBin/launchdhook/src/spawn_hook.m
+++ BaseBin/launchdhook/src/spawn_hook.m
@@ -37,11 +37,44 @@ int posix_spawn_hook(pid_t *restrict pid, const char *restrict path,
 			// But before, we want to pass the primitives to boomerang
 			boomerang_userspaceRebootIncoming();
 
+			//FILE *f = fopen("/var/mobile/launch_log.txt", "a");
+			//fprintf(f, "--- USERSPACE REBOOT ---\n");
+			//fclose(f);
+
 			// Say goodbye to this process
 			int (*orig)(pid_t *restrict, const char *restrict, const posix_spawn_file_actions_t *restrict, const posix_spawnattr_t *restrict, char *const[restrict], char *const[restrict]) = posix_spawn_orig;
 			return orig(pid, path, file_actions, attrp, argv, envp);
 		}
 	}
+
+	/*if (path) {
+		const char *firstArg = "<none>";
+		if (argv[0]) {
+			if (argv[1]) {
+				firstArg = argv[1];
+			}
+		}
+		FILE *f = fopen("/var/mobile/launch_log.txt", "a");
+		fprintf(f, "posix_spawn %s %s\n", path, firstArg);
+		fclose(f);
+
+		if (!strcmp(path, "/usr/libexec/xpcproxy")) {
+			const char *tmpBlacklist[] = {
+				"com.apple.logd"
+			};
+			size_t blacklistCount = sizeof(tmpBlacklist) / sizeof(tmpBlacklist[0]);
+			for (size_t i = 0; i < blacklistCount; i++)
+			{
+				if (!strcmp(tmpBlacklist[i], firstArg)) {
+					FILE *f = fopen("/var/mobile/launch_log.txt", "a");
+					fprintf(f, "blocked injection %s\n", firstArg);
+					fclose(f);
+					int (*orig)(pid_t *restrict, const char *restrict, const posix_spawn_file_actions_t *restrict, const posix_spawnattr_t *restrict, char *const[restrict], char *const[restrict]) = posix_spawn_orig;
+					return orig(pid, path, file_actions, attrp, argv, envp);
+				}
+			}
+		}
+	}*/
 
 	return spawn_hook_common(pid, path, file_actions, attrp, argv, envp, posix_spawn_orig);
 }
