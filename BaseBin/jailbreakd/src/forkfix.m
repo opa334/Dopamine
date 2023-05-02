b69b143903b1c6d3b78f3dae44fe1c74f7372150
opa334 <opa334@users.noreply.github.com>
2023-05-02 13:12:01 +0200

    Remove unused code

--- BaseBin/jailbreakd/src/forkfix.m
+++ BaseBin/jailbreakd/src/forkfix.m
@@ -15,44 +15,6 @@ typedef struct {
 } mem_region_info_t;
 extern kern_return_t mach_vm_region_recurse(vm_map_read_t target_task, mach_vm_address_t *address, mach_vm_size_t *size, natural_t *nesting_depth, vm_region_recurse_info_t info, mach_msg_type_number_t *infoCnt);
 
-mem_region_info_t *dump_regions(task_t task, int *region_count_out)
-{
-	mem_region_info_t *regions = (mem_region_info_t *)malloc(sizeof(mem_region_info_t));
-	int region_count = 0;
-	int max_regions = 1;
-
-	mach_vm_address_t start = 0x0;
-	int depth = 64;
-	while (1) {
-		if (region_count >= max_regions) {
-			max_regions *= 2;
-			regions = (mem_region_info_t *)realloc(regions, max_regions * sizeof(mem_region_info_t));
-		}
-
-		mach_vm_address_t address = start;
-		mach_vm_size_t size = 0;
-		uint32_t depth0 = depth;
-		vm_region_submap_info_data_64_t info;
-		mach_msg_type_number_t count = VM_REGION_SUBMAP_INFO_COUNT_64;
-		kern_return_t kr = mach_vm_region_recurse(task, &address, &size, &depth0, (vm_region_recurse_info_t)&info, &count);
-		if (kr != KERN_SUCCESS) {
-			break;
-		}
-
-		// Store the memory region information in the array
-		regions[region_count].address = address;
-		regions[region_count].size = size;
-		regions[region_count].prot = info.protection;
-		regions[region_count].max_prot = info.max_protection;
-		region_count++;
-
-		start = address + size;
-	}
-
-	if (region_count_out) *region_count_out = region_count;
-	return regions;
-}
-
 int64_t apply_fork_fixup(pid_t parentPid, pid_t childPid, bool mightHaveDirtyPages)
 {
 	int retval = 3;
