18fa915b9cde9a7c551421a66b97d745385b3d87
wwg135 <105048516+wwg135@users.noreply.github.com>
2023-05-21 16:46:17 +0800

    WIFI

--- Dopamine/Dopamine/UI/Views/Jailbreak.swift
+++ Dopamine/Dopamine/UI/Views/Jailbreak.swift
@@ -105,15 +105,24 @@ func isBootstrapped() -> Bool {
 
 func jailbreak(completion: @escaping (Error?) -> ()) {
     do {
+        var wifiFixupNeeded = false
+        var sleepNeeded = false
         if #available(iOS 15.4, *) {
             // No Wifi fixup needed
         }
         else {
-            if wifiIsEnabled() {
-                setWifiEnabled(false)
-                Logger.log("Disabling Wi-Fi", isStatus: true)
-                sleep(5)
-            }
+           wifiFixupNeeded = wifiIsEnabled()
+            sleepNeeded = true
+        }
+
+        if wifiFixupNeeded {
+            setWifiEnabled(false)
+            Logger.log("Disabling Wi-Fi", isStatus: true)
+        }
+
+        if sleepNeeded {
+            Logger.log("Log_Start_Jailbreaking", isStatus: true)
+            sleep(5)
         }
 
         Logger.log("Launching kexploitd", isStatus: true)
