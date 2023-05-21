64367733a5f41188c59fa742378f02c2d756b30b
wwg135 <105048516+wwg135@users.noreply.github.com>
2023-05-21 16:35:28 +0800

    add reboot

--- Dopamine/Dopamine/UI/Views/Jailbreak.swift
+++ Dopamine/Dopamine/UI/Views/Jailbreak.swift
@@ -65,6 +65,30 @@ func reboot() {
     _ = execCmd(args: [CommandLine.arguments[0], "reboot"])
 }
 
+func doReboot() {
+    UIImpactFeedbackGenerator(style: .soft).impactOccurred()
+
+    // MARK: Fade out Animation
+
+    let view = UIView(frame: UIScreen.main.bounds)
+    view.backgroundColor = .black
+    view.alpha = 0
+
+    for window in UIApplication.shared.connectedScenes.map({ $0 as? UIWindowScene }).compactMap({ $0 }).flatMap({ $0.windows.map { $0 } }) {
+        window.addSubview(view)
+        UIView.animate(withDuration: 0.2, delay: 0, animations: {
+            view.alpha = 1
+        })
+    }
+
+    DispatchQueue.main.asyncAfter(deadline: .now() + 0.2, execute: {
+        guard let rebootPath = rootifyPath(path: "/usr/sbin/reboot") else {
+            return
+        }
+        _ = execCmd(args: [rebootPath])
+    })
+}
+
 func isJailbroken() -> Bool {
     if isSandboxed() { return false } // ui debugging
     
