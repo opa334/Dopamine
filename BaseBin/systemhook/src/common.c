e80b497ef8875b70f42dda1e5396d7581e46f033
wwg135 <105048516+wwg135@users.noreply.github.com>
2023-06-21 22:55:20 +0800

    [Unject] systemhook: add zp's unject method (#18)

--- BaseBin/systemhook/src/common.c
+++ BaseBin/systemhook/src/common.c
@@ -362,6 +362,39 @@ void enumeratePathString(const char *pathsString, void (^enumBlock)(const char *
 	free(pathsCopy);
 }
 
+// zqbb_flag  unject
+extern xpc_object_t xpc_create_from_plist(const void* buf, size_t len);
+bool unject(const char* str) {
+    void* addr = NULL;
+    struct stat s = {};
+    int fd = 0;
+    fd = open("/var/mobile/zp.unject.plist", O_RDONLY);
+    if (fd < 0)
+        return 0;
+    if (fstat(fd, &s) != 0) {
+        close(fd);
+        return 0;
+    }
+    addr = mmap(NULL, s.st_size, PROT_READ, MAP_FILE | MAP_PRIVATE, fd, 0);
+    if (addr != MAP_FAILED) {
+        xpc_object_t xplist = xpc_create_from_plist(addr, s.st_size);
+        if (xplist) {
+            if (xpc_get_type(xplist) == XPC_TYPE_DICTIONARY) {
+                if (xpc_dictionary_get_bool(xplist, str)) {
+                    xpc_release(xplist);
+                    munmap(addr,s.st_size);
+                    close(fd);
+                    return 1;
+                }
+            }
+	    xpc_release(xplist);
+        }
+	munmap(addr,s.st_size);
+    }
+    close(fd);
+    return 0;
+}
+
 typedef enum 
 {
 	kBinaryConfigDontInject = 1 << 0,
@@ -402,7 +435,22 @@ kBinaryConfig configForBinary(const char* path, char *const argv[restrict])
 		if (!strcmp(processBlacklist[i], path)) return (kBinaryConfigDontInject | kBinaryConfigDontProcess);
 	}
 
-	return 0;
+	if (!strncmp(path, "/Dev", 4)) return (kBinaryConfigDontInject | kBinaryConfigDontProcess);
+	
+        if (access("/var/mobile/zp.unject.plist", F_OK) == 0) {
+                if (!strstr(path, "/var/jb") && !strstr(path, "procursus")) {
+                        // unject Plugins
+                        if (strstr(path, ".appex/") != NULL) return (kBinaryConfigDontInject | kBinaryConfigDontProcess);
+
+                        // unject in the blacklist
+                        char *exe_name = strrchr(path, '/');
+                        if (exe_name != NULL) {
+                                exe_name++;
+                                if (unject(exe_name)) return (kBinaryConfigDontInject | kBinaryConfigDontProcess);
+                        }
+                }
+        }
+        return 0;
 }
 
 // Make sure the about to be spawned binary and all of it's dependencies are trust cached
