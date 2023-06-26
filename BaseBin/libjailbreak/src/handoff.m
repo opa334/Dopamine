394f8a6d2fed1c1a2c8e79316b40dd6a9cd3618d
wwg135 <105048516+wwg135@users.noreply.github.com>
2023-06-26 11:16:00 +0800

    Update handoff.m

--- BaseBin/libjailbreak/src/handoff.m
+++ BaseBin/libjailbreak/src/handoff.m
@@ -6,7 +6,6 @@
 
 #define PERM_KRW_URW 0x7 // R/W for kernel and user
 #define FAKE_PHYSPAGE_TO_MAP 0x13370000
-#define PPL_MAP_ADDR         0x2000000 // This is essentially guaranteed to be unused, minimum address is usually 0x100000000
 
 kern_return_t pmap_enter_options_addr(uint64_t pmap, uint64_t pa, uint64_t va) {
     while (1) {
@@ -21,9 +20,68 @@ void pmap_remove(uint64_t pmap, uint64_t start, uint64_t end) {
     kcall8(bootInfo_getSlidUInt64(@"pmap_remove_options"), pmap, start, end, 0x100, 0, 0, 0, 0);
 }
 
-int handoffPPLPrimitives(pid_t pid, uint64_t *magicPageOut)
+int pmap_map_in(uint64_t pmap, uint64_t uaddr, uint64_t pa, uint64_t size)
 {
-	if (!pid || !magicPageOut) return -1;
+	uint64_t mappingSize = 0x4000 * 2048;
+	uint64_t mappingUaddr = uaddr - (uaddr % mappingSize);
+	uint64_t mappingPA = pa - (pa % mappingSize);
+
+	uint64_t pageCount = size / 0x4000;
+	uint64_t mappingCount = (pageCount / 2048) + ((pageCount % 2048) != 0);
+
+	for (uint64_t i = 0; i < mappingCount; i++) {
+		uint64_t curMappingUaddr = mappingUaddr + (i * mappingSize);
+		kern_return_t kr = pmap_enter_options_addr(pmap, FAKE_PHYSPAGE_TO_MAP, curMappingUaddr);
+		if (kr != KERN_SUCCESS) {
+			pmap_remove(pmap, mappingUaddr, curMappingUaddr);
+			return -7;
+		}
+	}
+
+	// Temporarily change pmap type to nested
+	pmap_set_type(pmap, 3);
+	// Remove mapping (table will not be removed because we changed the pmap type)
+	pmap_remove(pmap, mappingUaddr, mappingUaddr + (mappingCount * mappingSize));
+	// Change type back
+	pmap_set_type(pmap, 0);
+
+	for (uint64_t i = 0; i < mappingCount; i++) {
+		uint64_t curMappingUaddr = mappingUaddr + (i * mappingSize);
+		uint64_t curMappingPA = mappingPA + (i * mappingSize);
+
+		// Create full table for this mapping
+		uint64_t tableToWrite[2048];
+		for (int k = 0; k < 2048; k++) {
+			uint64_t curMappingPage = curMappingPA + (k * 0x4000);
+			if (curMappingPage >= pa || curMappingPage < (pa + size)) {
+				tableToWrite[k] = curMappingPage | PERM_TO_PTE(PERM_KRW_URW) | PTE_NON_GLOBAL | PTE_OUTER_SHAREABLE | PTE_LEVEL3_ENTRY;
+			}
+			else {
+				tableToWrite[k] = 0;
+			}
+		}
+
+		// Replace table with the entries we generated
+		uint64_t table2Entry = pmap_lv2(pmap, curMappingUaddr);
+		if ((table2Entry & 0x3) == 0x3) {
+			uint64_t table3 = table2Entry & 0xFFFFFFFFC000ULL;
+			physwritebuf(table3, tableToWrite, 0x4000);
+
+			usleep(70);
+			usleep(70);
+			__asm("dmb sy");
+		}
+		else {
+			return -6;
+		}
+	}
+
+	return 0;
+}
+
+int handoffPPLPrimitives(pid_t pid)
+{
+	if (!pid) return -1;
 
 	int ret = 0;
 
@@ -36,29 +94,10 @@ int handoffPPLPrimitives(pid_t pid, uint64_t *magicPageOut)
 			if (vmMap) {
 				uint64_t pmap = vm_map_get_pmap(vmMap);
 				if (pmap) {
-					kern_return_t kr = pmap_enter_options_addr(pmap, FAKE_PHYSPAGE_TO_MAP, PPL_MAP_ADDR);
-					if (kr == KERN_SUCCESS) {
-						// Temporarily change pmap type to nested
-						pmap_set_type(pmap, 3);
-
-						// Remove mapping (table will not be removed because we changed the pmap type)
-						pmap_remove(pmap, PPL_MAP_ADDR, PPL_MAP_ADDR + 0x4000);
-
-						// Change type back
-						pmap_set_type(pmap, 0);
-
-						// Change the mapping to map the underlying page table
-						uint64_t table2Entry = pmap_lv2(pmap, PPL_MAP_ADDR);
-						if ((table2Entry & 0x3) == 0x3) {
-							uint64_t table3 = table2Entry & 0xFFFFFFFFC000ULL;
-							uint64_t pte = table3 | PERM_TO_PTE(PERM_KRW_URW) | PTE_NON_GLOBAL | PTE_OUTER_SHAREABLE | PTE_LEVEL3_ENTRY;
-							physwrite64(table3, pte);
-
-							*magicPageOut = PPL_MAP_ADDR;
-						}
-						else { ret = -7; }
-					}
-					else { ret = -6; }
+					// Map the entire kernel physical address space into the userland process 1:1
+					uint64_t physBase = kread64(bootInfo_getSlidUInt64(@"gPhysBase"));
+					uint64_t physSize = kread64(bootInfo_getSlidUInt64(@"gPhysSize"));
+					ret = pmap_map_in(pmap, physBase, physBase, physSize);
 				}
 				else { ret = -5; }
 			}
