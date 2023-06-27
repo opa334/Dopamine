3ee59662554d7fe7062b4c6860f4dde9fda80f9c
wwg135 <105048516+wwg135@users.noreply.github.com>
2023-06-28 06:57:16 +0800

    Delete mount

--- BaseBin/jailbreakd/src/fakelib.h
+++ BaseBin/jailbreakd/src/fakelib.h
@@ -2,4 +2,3 @@ int setFakeLibVisible(bool visible);
 int makeFakeLib(void);
 bool isFakeLibBindMountActive(void);
 int setFakeLibBindMountActive(bool active);
-void initMountPath(NSString *mountPath, bool new);
