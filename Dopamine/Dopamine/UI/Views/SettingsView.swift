af062d2ad665bf0540fa7f7f8a4746e5b3c8b9bb
wwg135 <105048516+wwg135@users.noreply.github.com>
2023-06-29 20:47:47 +0800

    Update opacticy

--- Dopamine/Dopamine/UI/Views/SettingsView.swift
+++ Dopamine/Dopamine/UI/Views/SettingsView.swift
@@ -182,11 +182,11 @@ struct SettingsView: View {
                     VStack(spacing: 6) {
                         Text(isBootstrapped() ? "Settings_Footer_Device_Bootstrapped" :  "Settings_Footer_Device_Not_Bootstrapped")
                             .font(.footnote)
-                            .opacity(0.6)
+                            .opacity(1)
                         if isJailbroken() {
                             Text("Success_Rate \(successRate())% (\(successfulJailbreaks)/\(totalJailbreaks))")
                                 .font(.footnote)
-                                .opacity(0.6)
+                                .opacity(1)
                         }
                     }
                     .padding(.top, 2)
