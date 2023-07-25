ee0fec913097c427b085bb53f02369d67a1c08bc
opa334 <opa334@users.noreply.github.com>
2023-07-26 01:24:41 +0200

    Fix wrong math

--- BaseBin/libjailbreak/src/handoff.m
+++ BaseBin/libjailbreak/src/handoff.m
@@ -6,6 +6,9 @@
 
 #define PERM_KRW_URW 0x7 // R/W for kernel and user
 #define FAKE_PHYSPAGE_TO_MAP 0x13370000
+#define L2_MAPPING_SIZE 0x2000000
+#define L2_MAPPING_PAGECOUNT (L2_MAPPING_SIZE / PAGE_SIZE)
+#define L2_MAPPING_MASK (L2_MAPPING_SIZE-1)
 
 uint64_t pmap_alloc_page_for_kern(unsigned int options)
 {
@@ -48,17 +51,18 @@ void pmap_remove(uint64_t pmap, uint64_t start, uint64_t end) {
     kcall8(bootInfo_getSlidUInt64(@"pmap_remove_options"), pmap, start, end, 0x100, 0, 0, 0, 0);
 }
 
-int pmap_map_in(uint64_t pmap, uint64_t uaddr, uint64_t pa, uint64_t size)
+int pmap_map_in(uint64_t pmap, uint64_t ua, uint64_t pa, uint64_t size)
 {
-	uint64_t mappingSize = 0x4000 * 2048;
-	uint64_t mappingUaddr = uaddr - (uaddr % mappingSize);
-	uint64_t mappingPA = pa - (pa % mappingSize);
+	uint64_t mappingUaddr = ua & ~L2_MAPPING_MASK;
+	uint64_t mappingPA = pa & ~L2_MAPPING_MASK;
 
-	uint64_t pageCount = size / 0x4000;
-	uint64_t mappingCount = (pageCount / 2048) + ((pageCount % 2048) != 0);
+	uint64_t endPA = pa + size;
+	uint64_t mappingEndPA = endPA & ~L2_MAPPING_MASK;
+
+	uint64_t mappingCount = ((mappingEndPA - mappingPA) / L2_MAPPING_SIZE) + 1;
 
 	for (uint64_t i = 0; i < mappingCount; i++) {
-		uint64_t curMappingUaddr = mappingUaddr + (i * mappingSize);
+		uint64_t curMappingUaddr = mappingUaddr + (i * L2_MAPPING_SIZE);
 		kern_return_t kr = pmap_enter_options_addr(pmap, FAKE_PHYSPAGE_TO_MAP, curMappingUaddr);
 		if (kr != KERN_SUCCESS) {
 			pmap_remove(pmap, mappingUaddr, curMappingUaddr);
@@ -69,13 +73,13 @@ int pmap_map_in(uint64_t pmap, uint64_t uaddr, uint64_t pa, uint64_t size)
 	// Temporarily change pmap type to nested
 	pmap_set_type(pmap, 3);
 	// Remove mapping (table will not be removed because we changed the pmap type)
-	pmap_remove(pmap, mappingUaddr, mappingUaddr + (mappingCount * mappingSize));
+	pmap_remove(pmap, mappingUaddr, mappingUaddr + (mappingCount * L2_MAPPING_SIZE));
 	// Change type back
 	pmap_set_type(pmap, 0);
 
 	for (uint64_t i = 0; i < mappingCount; i++) {
-		uint64_t curMappingUaddr = mappingUaddr + (i * mappingSize);
-		uint64_t curMappingPA = mappingPA + (i * mappingSize);
+		uint64_t curMappingUaddr = mappingUaddr + (i * L2_MAPPING_SIZE);
+		uint64_t curMappingPA = mappingPA + (i * L2_MAPPING_SIZE);
 
 		// Create full table for this mapping
 		uint64_t tableToWrite[2048];
@@ -94,10 +98,6 @@ int pmap_map_in(uint64_t pmap, uint64_t uaddr, uint64_t pa, uint64_t size)
 		if ((table2Entry & 0x3) == 0x3) {
 			uint64_t table3 = table2Entry & 0xFFFFFFFFC000ULL;
 			physwritebuf(table3, tableToWrite, 0x4000);
-
-			usleep(70);
-			usleep(70);
-			__asm("dmb sy");
 		}
 		else {
 			return -6;
