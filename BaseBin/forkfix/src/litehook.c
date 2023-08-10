ba7f35aa4b84bc627400fbb55ad5e7bcb5977565
opa334 <opa334@users.noreply.github.com>
2023-08-11 01:23:12 +0200

    Attempt to workaround spinlock panics (not working, but does seem to reduce them)

--- BaseBin/forkfix/src/litehook.c
+++ BaseBin/forkfix/src/litehook.c
@@ -1,4 +1,16 @@
 #include "litehook.h"
+#include <stdarg.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <string.h>
+#include <sys/fcntl.h>
+#include <mach/mach.h>
+#include <mach/arm/kern_return.h>
+#include <mach/port.h>
+#include <mach/vm_prot.h>
+#include <mach-o/dyld.h>
+#include <dlfcn.h>
+#include <libkern/OSCacheControl.h>
 
 static uint64_t __attribute((naked)) __xpaci(uint64_t a)
 {
@@ -45,9 +57,9 @@ uint32_t br(uint8_t x)
 
 __attribute__((noinline, naked)) volatile kern_return_t litehook_vm_protect(mach_port_name_t target, mach_vm_address_t address, mach_vm_size_t size, boolean_t set_maximum, vm_prot_t new_protection)
 {
-    __asm("mov x16, #0xFFFFFFFFFFFFFFF2");
-    __asm("svc 0x80");
-    __asm("ret");
+	__asm("mov x16, #0xFFFFFFFFFFFFFFF2");
+	__asm("svc 0x80");
+	__asm("ret");
 }
 
 kern_return_t litehook_unprotect(vm_address_t addr, vm_size_t size)
@@ -60,12 +72,78 @@ kern_return_t litehook_protect(vm_address_t addr, vm_size_t size)
 	return litehook_vm_protect(mach_task_self(), addr, size, false, VM_PROT_READ | VM_PROT_EXECUTE);
 }
 
+int _dyld_image_index_for_header(const void *header)
+{
+	for (int i = 0; i < _dyld_image_count(); i++) {
+		const struct mach_header *checkHeader = _dyld_get_image_header(i);
+		if (header == checkHeader) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+int getSectionBounds(const void *address, mach_vm_address_t *startOut, mach_vm_address_t *endOut) {
+	Dl_info info;
+	int dlr = dladdr((void *)address, &info);
+	if (dlr == 0) return 1;
+	const struct mach_header_64 *header = info.dli_fbase;
+
+	int imageIndex = _dyld_image_index_for_header(header);
+	
+	if (header && imageIndex != -1) {
+		intptr_t slide = _dyld_get_image_vmaddr_slide(imageIndex);
+		uint64_t unslidAddress = ((uint64_t)address) - slide;
+		
+		const struct segment_command_64 *segmentCmd = NULL;
+		uint32_t segmentCount = 0;
+		if (header->magic == MH_MAGIC || header->magic == MH_MAGIC_64) {
+			segmentCmd = (const struct segment_command_64 *)(((const char *)header) + sizeof(struct mach_header_64));
+			segmentCount = header->ncmds;
+		}
+		
+		for (uint32_t i = 0; i < segmentCount; i++) {
+			if (segmentCmd->cmd == LC_SEGMENT || segmentCmd->cmd == LC_SEGMENT_64) {
+				if (unslidAddress >= segmentCmd->vmaddr &&
+					unslidAddress < segmentCmd->vmaddr + segmentCmd->vmsize) {
+					mach_vm_address_t subsectionStart = segmentCmd->vmaddr + slide;
+					mach_vm_address_t subsectionEnd = segmentCmd->vmsize;
+					*startOut = subsectionStart;
+					*endOut = subsectionEnd;
+					return 0;
+				}
+			}
+			segmentCmd = (const struct segment_command_64 *)(((const char *)segmentCmd) + segmentCmd->cmdsize);
+		}
+	}
+
+	return 1;
+}
+
 kern_return_t litehook_hook_function(void *source, void *target)
 {
+	kern_return_t kr = KERN_SUCCESS;
+
+	mach_vm_address_t regionStart = 0;
+	mach_vm_address_t regionSize = 0;
+	int suc = getSectionBounds(source, &regionStart, &regionSize);
+	if (suc != 0) return suc;
+
+	vm_address_t preWarmAllocation = 0;
+	kr = vm_allocate(mach_task_self_, &preWarmAllocation, regionSize*2, VM_FLAGS_ANYWHERE);
+	if (kr != KERN_SUCCESS) return kr;
+	vm_address_t preWarmAllocationEnd = preWarmAllocation + (regionSize*2);
+	for (vm_address_t page = preWarmAllocation; page < preWarmAllocationEnd; page += PAGE_SIZE) {
+		// page in
+		*((volatile uint64_t *)page);
+	}
+	kr = vm_deallocate(mach_task_self_, preWarmAllocation, regionSize*2);
+	if (kr != KERN_SUCCESS) return kr;
+
 	uint32_t *toHook = (uint32_t*)xpaci((uint64_t)source);
 	uint64_t target64 = (uint64_t)xpaci((uint64_t)target);
 
-	kern_return_t kr = litehook_unprotect((vm_address_t)toHook, 5*4);
+	kr = litehook_unprotect((vm_address_t)toHook, 5*4);
 	if (kr != KERN_SUCCESS) return kr;
 
 	toHook[0] = movk(16, target64 >> 0, 0);
@@ -76,6 +154,13 @@ kern_return_t litehook_hook_function(void *source, void *target)
 
 	kr = litehook_protect((vm_address_t)toHook, 5*4);
 	if (kr != KERN_SUCCESS) return kr;
+
+	sys_icache_invalidate(source, VM_PAGE_SIZE);
+
+	for (mach_vm_address_t page = regionStart; page < regionSize; page += PAGE_SIZE) {
+		// page in
+		*((volatile uint64_t *)page);
+	}
 
 	return KERN_SUCCESS;
 }
