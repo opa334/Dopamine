33cb2a4941a9989aad2d19de762acf1deb5d2449
opa334 <opa334@users.noreply.github.com>
2023-08-14 00:06:47 +0200

    Was worth a try I guess?

--- BaseBin/forkfix/src/litehook.c
+++ BaseBin/forkfix/src/litehook.c
@@ -72,77 +72,12 @@ kern_return_t litehook_protect(vm_address_t addr, vm_size_t size)
 	return litehook_vm_protect(mach_task_self(), addr, size, false, VM_PROT_READ | VM_PROT_EXECUTE);
 }
 
-int _dyld_image_index_for_header(const void *header)
-{
-	for (int i = 0; i < _dyld_image_count(); i++) {
-		const struct mach_header *checkHeader = _dyld_get_image_header(i);
-		if (header == checkHeader) {
-			return i;
-		}
-	}
-	return -1;
-}
-
-int getSectionBounds(const void *address, mach_vm_address_t *startOut, mach_vm_address_t *endOut) {
-	Dl_info info;
-	int dlr = dladdr((void *)address, &info);
-	if (dlr == 0) return 1;
-	const struct mach_header_64 *header = info.dli_fbase;
-
-	int imageIndex = _dyld_image_index_for_header(header);
-	
-	if (header && imageIndex != -1) {
-		intptr_t slide = _dyld_get_image_vmaddr_slide(imageIndex);
-		uint64_t unslidAddress = ((uint64_t)address) - slide;
-		
-		const struct segment_command_64 *segmentCmd = NULL;
-		uint32_t segmentCount = 0;
-		if (header->magic == MH_MAGIC || header->magic == MH_MAGIC_64) {
-			segmentCmd = (const struct segment_command_64 *)(((const char *)header) + sizeof(struct mach_header_64));
-			segmentCount = header->ncmds;
-		}
-		
-		for (uint32_t i = 0; i < segmentCount; i++) {
-			if (segmentCmd->cmd == LC_SEGMENT || segmentCmd->cmd == LC_SEGMENT_64) {
-				if (unslidAddress >= segmentCmd->vmaddr &&
-					unslidAddress < segmentCmd->vmaddr + segmentCmd->vmsize) {
-					mach_vm_address_t subsectionStart = segmentCmd->vmaddr + slide;
-					mach_vm_address_t subsectionEnd = segmentCmd->vmsize;
-					*startOut = subsectionStart;
-					*endOut = subsectionEnd;
-					return 0;
-				}
-			}
-			segmentCmd = (const struct segment_command_64 *)(((const char *)segmentCmd) + segmentCmd->cmdsize);
-		}
-	}
-
-	return 1;
-}
-
 kern_return_t litehook_hook_function(void *source, void *target)
 {
 	kern_return_t kr = KERN_SUCCESS;
 
 	uint32_t *toHook = (uint32_t*)xpaci((uint64_t)source);
 	uint64_t target64 = (uint64_t)xpaci((uint64_t)target);
-
-	mach_vm_address_t regionStart = 0;
-	mach_vm_address_t regionSize = 0;
-	int suc = getSectionBounds(toHook, &regionStart, &regionSize);
-	if (suc != 0) return suc;
-
-	vm_address_t preWarmAllocation = 0;
-	kr = vm_allocate(mach_task_self_, &preWarmAllocation, regionSize*2, VM_FLAGS_ANYWHERE);
-	if (kr != KERN_SUCCESS) return kr;
-	vm_address_t preWarmAllocationEnd = preWarmAllocation + (regionSize*2);
-	for (vm_address_t page = preWarmAllocation; page < preWarmAllocationEnd; page += PAGE_SIZE) {
-		// page in
-		*((volatile uint64_t *)page);
-	}
-	kr = vm_deallocate(mach_task_self_, preWarmAllocation, regionSize*2);
-	if (kr != KERN_SUCCESS) return kr;
-
 
 	kr = litehook_unprotect((vm_address_t)toHook, 5*4);
 	if (kr != KERN_SUCCESS) return kr;
@@ -158,11 +93,6 @@ kern_return_t litehook_hook_function(void *source, void *target)
 	if (kr != KERN_SUCCESS) return kr;
 
 	sys_icache_invalidate(toHook, hookSize);
-
-	for (mach_vm_address_t page = regionStart; page < regionSize; page += PAGE_SIZE) {
-		// page in
-		*((volatile uint64_t *)page);
-	}
 
 	return KERN_SUCCESS;
 }
