0bc7b99d9615158673ec6a4d6af0f7b752f4f09e
RootHide <134120506+RootHide@users.noreply.github.com>
2023-10-15 04:10:12 +0800

    fix finding arch for mach-o (#265)
    * fix finding arch for mach-o (some fat mach-o may contain mach-o slices of both arm64e.v1 and arm64e.v2)
    
    * fix parameter and environment variable passing issues of exec*
    
    * fix PATH for execvP

--- BaseBin/systemhook/src/common.c
+++ BaseBin/systemhook/src/common.c
@@ -253,8 +253,13 @@ int resolvePath(const char *file, const char *searchPath, int (^attemptHandler)(
 	struct stat sb;
 	char path_buf[PATH_MAX];
 
-	if ((env_path = getenv("PATH")) == NULL)
-		env_path = _PATH_DEFPATH;
+	env_path = searchPath;
+	if (!env_path) {
+		env_path = getenv("PATH");
+		if (!env_path) {
+			env_path = _PATH_DEFPATH;
+		}
+	}
 
 	/* If it's an absolute or relative path name, it's easy. */
 	if (index(file, '/')) {
