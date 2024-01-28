4d46fe0ce39d7993430512d099c08c3a7de6b1fa
tomt000 <tomt000@icloud.com>
2024-01-28 18:28:33 +0100

    Removed duplicated code

--- Application/Dopamine/UI/Settings/DOSettingsController.m
+++ Application/Dopamine/UI/Settings/DOSettingsController.m
@@ -16,17 +16,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    self.overrideUserInterfaceStyle = UIUserInterfaceStyleDark;
-    self.view.backgroundColor = [UIColor colorWithWhite:0.3 alpha:0.6];
-    self.view.layer.cornerRadius = 16;
-    self.view.layer.masksToBounds = YES;
-    self.view.layer.cornerCurve = kCACornerCurveContinuous;
-    
-    [_table setSeparatorColor:[UIColor clearColor]];
-    [_table setBackgroundColor:[UIColor clearColor]];
-    
-    [UISwitch appearanceWhenContainedInInstancesOfClasses:@[[self class]]].onTintColor = [UIColor colorWithRed: 71.0/255.0 green: 169.0/255.0 blue: 135.0/255.0 alpha: 1.0];
 }
 
 - (id)specifiers {
