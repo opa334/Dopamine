bbaf4d6b382bd25892f48141211674f9eb88b9aa
khcrysalis <97859147+khcrysalis@users.noreply.github.com>
2025-04-14 16:16:43 -0700

    chore: allow biometrics

--- Application/Dopamine/UI/Settings/DOSettingsController.m
+++ Application/Dopamine/UI/Settings/DOSettingsController.m
@@ -479,8 +479,8 @@
 	NSError *authError = nil;
 	NSString *reason = DOLocalizedString(@"Password_Auth_Required");
 	
-	if ([context canEvaluatePolicy:LAPolicyDeviceOwnerAuthentication error:&authError]) {
-		[context evaluatePolicy:LAPolicyDeviceOwnerAuthentication
+	if ([context canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics error:&authError]) {
+		[context evaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics
 			localizedReason:reason
 			reply:^(BOOL success, NSError * _Nullable error) {
 			dispatch_async(dispatch_get_main_queue(), ^{
